d9c053c2d83a725f33e904e2ebbf5781ffe86604
Kevin <kchen4000@gmail.com>
2020-12-02 00:17:12 -0800

    updated export.java, created Cowdance.java and its data

--- assignments/export.java
+++ assignments/export.java
@@ -1,49 +1,63 @@
 import java.util.*;
 import java.io.*;
 public class export {
+	static int n;
+	static int Tmax;
+	static int[] times;
 	public static void main(String[] args) throws IOException{
-		BufferedReader in = new BufferedReader(new FileReader("shuffle.in"));
+		BufferedReader in = new BufferedReader(new FileReader("cowdance.in"));
 		StringTokenizer st = new StringTokenizer(in.readLine());
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("shuffle.out")));
-		int n = Integer.parseInt(st.nextToken());
-		Map<Integer, Integer> shuffle = new HashMap<Integer, Integer>();
-		Set<Integer> cycle = new HashSet<Integer>();
-		Set<Integer> uncycle = new HashSet<Integer>();
-		st = new StringTokenizer(in.readLine());
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("cowdance.out")));
+		n = Integer.parseInt(st.nextToken());
+		Tmax = Integer.parseInt(st.nextToken());
+		times = new int[n];
 		for(int i = 0; i < n; i++) {
-			int temp = Integer.parseInt(st.nextToken());
-			shuffle.put(i + 1, temp);
+			st = new StringTokenizer(in.readLine());
+			times[i] = Integer.parseInt(st.nextToken());
 		}
-		for(int i = 1; i <= n; i++) {
-			if(uncycle.contains(i) || cycle.contains(i)) {
-				continue;
-			}			
-			Set<Integer> current = new HashSet<Integer>();
-			current.add(i);
-			int temp = shuffle.get(i);
-			while(true) {
-				if(uncycle.contains(temp) || cycle.contains(temp)) {
-					uncycle.add(i);
-					break;
-				}	
-				current.add(temp);
-				if(!current.contains(shuffle.get(temp))) {
-					temp = shuffle.get(temp);
-				}
-				else {
-					if(shuffle.get(temp) == i) {
-						cycle.addAll(current);
-						break;
-					}
-					else {
-						uncycle.add(i);
-						break;
-					}
-				}
+		int top = n;
+		int bot = 1;
+		while(top > bot) {
+			int mid = (top + bot) / 2;
+			if(check(mid)) {
+				top = mid;
+			}
+			else {
+				bot = mid + 1;
 			}
 		}
-		out.println(cycle.size());
+		out.println(top);
 		in.close();
 		out.close();
+	}
+	public static boolean check(int stage) {
+		TreeMap<Integer, Integer> cowsOnFloor = new TreeMap<Integer, Integer>();
+		for(int i = 0; i < stage; i++) {
+			if(cowsOnFloor.containsKey(times[i])) {
+				cowsOnFloor.put(times[i], cowsOnFloor.get(times[i]) + 1);
+			}
+			else {
+				cowsOnFloor.put(times[i], 1);
+			}
+		}
+		for(int i = stage; i < n; i++) {
+			int lowest = cowsOnFloor.firstKey();
+			if(cowsOnFloor.get(lowest) != 1) {
+				cowsOnFloor.put(lowest, cowsOnFloor.get(lowest) - 1);
+			}
+			else {
+				cowsOnFloor.remove(lowest);
+			}
+			if(cowsOnFloor.containsKey(lowest + times[i])) {
+				cowsOnFloor.put(times[i] + lowest, cowsOnFloor.get(times[i] + lowest) + 1);
+			}
+			else {
+				cowsOnFloor.put(lowest + times[i], 1);
+			}
+		}
+		if(cowsOnFloor.lastKey() > Tmax) {
+			return false;
+		}
+		return true;
 	}
 }
