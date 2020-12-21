dbe218ab85ad061f16456b01e4a03af47fc93324
Kevin <kchen4000@gmail.com>
2020-12-21 14:47:37 -0800

    updated export.java started Cowntagion.java 1/15 finished MooBuzz.java and its data

--- assignments/export.java
+++ assignments/export.java
@@ -1,72 +1,32 @@
 import java.util.*;
 import java.io.*;
 public class export {
-	long start;
-	long end;
 	static int n;
-	static int m;
-	static long smallest;
-	static long largest;
-	static TreeMap<Long, Long> fields;
 	public static void main(String[] args) throws IOException{
-		BufferedReader in = new BufferedReader(new FileReader("socdist.in"));
+		BufferedReader in = new BufferedReader(new FileReader("moobuzz.in"));
 		StringTokenizer st = new StringTokenizer(in.readLine());
-		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("socdist.out")));
+		PrintWriter out = new PrintWriter(new BufferedWriter(new FileWriter("moobuzz.out")));
 		n = Integer.parseInt(st.nextToken());
-		m = Integer.parseInt(st.nextToken());
-		smallest = Long.MAX_VALUE;
-		largest = Long.MIN_VALUE;
-		fields = new TreeMap<Long, Long>();
-		for(int i = 0; i < m; i++) {
-			st = new StringTokenizer(in.readLine());
-			long start = Long.parseLong(st.nextToken());
-			long end = Long.parseLong(st.nextToken());
-			fields.put(start, end);
-			if(start < smallest) {
-				smallest = start;
-			}
-			if(end > largest) {
-				largest = end;
-			}
-		}
-		long top = largest;
-		long bot = 1;
-		long mid = 0;
+		long top = Long.MAX_VALUE;
+		long bot = n;
 		while(top > bot) {
-			mid = (top + bot) / 2;
-			boolean temp = check(mid);
-			if(bot == mid && temp) {
-				out.println(bot);
-				break;
-			}
-			if(temp) {
-				bot = mid;
-			}
-			else {
+			long mid = (top + bot) / 2;
+			if(check(mid)) {
 				top = mid;
 			}
+			else {
+				bot = mid + 1;
+			}
 		}
+		out.println(top);
 		in.close();
 		out.close();
 	}
-	public static boolean check(long dist) {
-		long position = smallest;
-		int cows = n;
-		while(position <= largest && cows > 0) {
-			Long currentFieldStart = fields.floorKey(position);
-			Long currentFieldEnd = fields.get(currentFieldStart);
-			if(currentFieldStart <= position && position <= currentFieldEnd) {
-				cows--;
-				position += dist;
-			}
-			else {
-				if(fields.get(fields.lastKey()) < position) {
-					break;
-				}
-				position = fields.ceilingKey(position);
-			}
-		}
-		if(cows == 0) {
+	public static boolean check(long mid) {
+		long triple = mid / 3;
+		long five = mid / 5;
+		long fifthteenth = mid / 15;
+		if(mid - triple - five + fifthteenth >= n) {
 			return true;
 		}
 		return false;
